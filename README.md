7b362fa436fe51d917faced6ecaeef2d76c3e063
lukas.fischer <lukas.fischer@netnode.ch>
2024-08-11 02:02:57 +0200

    latest composer updates, README improvements, added features screenshot

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # NodeHive Headless CMS CE
 
-[<img src="https://netnode.nodehive.app/sites/default/files/2024-05/nodehive_spaces.png" style="max-width:800px">](https://www.nodehive.com)
-
 ## What is NodeHive Headless CMS?
 
 NodeHive Headless CMS is a headless composable platform that serves as your central commerce, marketing, communication and customer engagement hub. NodeHive is built on top of the wide spread [Drupal CMS](https://www.drupal.org) and extends Drupal with advanced headless features.
 
+[<img src="/features.png" style="max-width:800px;border: 2px solid #8EBAB6">](https://www.nodehive.com)
+
 This repository is the offical NodeHive Headless CMS Community Edition. The makers of NodeHive also offer [a paid SaaS version of NodeHive Headless CMS](https://www.nodehive.com/saas-pricing) as well as enterprise support.
 
-
+## Benefits
+- Easily manage all your websites from one place
+- Editing content is simple and fast
+- Customize your structured content just the way you want it
+- Plan out your content ahead of time
+- Make sure your content goes live at the right time
+- ... and much more
 
 ## Features
 - One backend, multiple websites
@@ -17,6 +23,8 @@ This repository is the offical NodeHive Headless CMS Community Edition. The make
 - Paul AI
 - Dashboards
 - Content Calendar
+- RAG / AI Chat (SaaS and enterprise only)
+- Content Projects (SaaS and enterprise only)
 - ... and much more
 
 ## Important links:
@@ -27,7 +35,7 @@ This repository is the offical NodeHive Headless CMS Community Edition. The make
 
 ## Installation
 
-### Install using DDEV
+### Install with DDEV
 
 Prerequisite: DDEV installed (https://ddev.readthedocs.io/en/stable/)
 
@@ -39,7 +47,7 @@ ddev install-nodehive-ce
 ```
 Your site is now available at https://nodehive-ce.ddev.site
 
-### Manual installation
+### Install with composer directly
 
 Prerequisite: A working LAMP stack
 
@@ -52,4 +60,3 @@ Adjust the `settings.php` so that Drupal can be installed [Drupal Docs](https://
 ```
 composer install-nodehive-ce
 ```
-Site should now be set-up correctly, you can access it via configured local url in your LAMP stack.
--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -3,7 +3,9 @@
 set -e
 
 # Color codes
+RED="\e[31m"
 GREEN="\033[0;32m"
+BOLDGREEN="\e[1;${GREEN}m"
 YELLOW="\033[1;33m"
 CYAN="\033[0;36m"
 NC="\033[0m" # No Color
@@ -14,6 +16,20 @@ DRUPAL_SETTINGS_FILE="./web/sites/default/settings.php"
 
 # Flag variables
 SKIP_COMPOSER=false
+
+cat << EOF
+
+#############################################################
+
+┏┓╻┏━┓╺┳┓┏━╸╻ ╻╻╻ ╻┏━╸   ╻ ╻┏━╸┏━┓╺┳┓╻  ┏━╸┏━┓┏━┓   ┏━╸┏┳┓┏━┓
+┃┗┫┃ ┃ ┃┃┣╸ ┣━┫┃┃┏┛┣╸    ┣━┫┣╸ ┣━┫ ┃┃┃  ┣╸ ┗━┓┗━┓   ┃  ┃┃┃┗━┓
+╹ ╹┗━┛╺┻┛┗━╸╹ ╹╹┗┛ ┗━╸   ╹ ╹┗━╸╹ ╹╺┻┛┗━╸┗━╸┗━┛┗━┛   ┗━╸╹ ╹┗━┛
+Scale your digital platform.
+
+#############################################################
+
+EOF
+
 
 # Parse command line options
 while [[ "$#" -gt 0 ]]; do
@@ -26,50 +42,66 @@ done
 
 echo -e "\r\n"
 if [ "$SKIP_COMPOSER" = false ]; then
-    echo -e "${GREEN}### RUN: composer install ###${NC}"
+    echo -e "${RED}### RUN: composer install ###${NC}"
     composer install --no-dev --no-interaction
     echo -e "${CYAN}### composer install -> DONE ###${NC}\n"
 fi
 
 echo -e "\r\n"
-echo -e "${GREEN}### RUN: Drupal install ###${NC}"
+echo -e "${RED}### RUN: Drupal install ###${NC}"
 ./vendor/bin/drush site:install --account-name=admin --account-pass=admin -y
 echo -e "${CYAN}### Drupal install -> DONE ###${NC}\n"
 
 echo -e "\r\n"
-echo -e "${GREEN}### RUN: Enable Beekeeper module ###${NC}"
+echo -e "${RED}### RUN: Enable Beekeeper module ###${NC}"
 ./vendor/bin/drush pm:enable nodehive_core_beekeeper -y
 echo -e "${CYAN}### Enable Beekeeper -> DONE ###${NC}\n"
 
 echo -e "\r\n"
-echo -e "${GREEN}### RUN: Install Nodehive CE module ###${NC}"
+echo -e "${RED}### RUN: Install NodeHive CE module ###${NC}"
 ./vendor/bin/drush pm-enable nodehive_ce -y
-echo -e "\r\n"
-echo -e "${CYAN}### Install Nodehive CE module -> DONE ###${NC}"
+echo -e "${CYAN}### Install NodeHive CE module -> DONE ###${NC}"
 
 echo -e "\r\n"
-echo -e "${GREEN}### RUN: Nodehive install ###${NC}"
+echo -e "${RED}### RUN: NodeHive install ###${NC}"
 ./vendor/bin/drush beekeeper:install
 ./vendor/bin/drush beekeeper:install --preset=nodehive-ce
-echo -e "\r\n"
-echo -e "${CYAN}### Nodehive install -> DONE ###${NC}"
+echo -e "${CYAN}### NodeHive install -> DONE ###${NC}"
 
 echo -e "\r\n"
-echo -e "${GREEN}### RUN: Set up JWT key ###${NC}"
+echo -e "${RED}### RUN: Set up JWT key ###${NC}"
 mkdir -p private
 openssl genrsa 2048 > ./private/jwt.key
 echo -e "${CYAN}### Set up JWT key -> DONE ###${NC}"
 
 echo -e "\r\n"
-echo -e "${GREEN}### RUN: Set up roles ###${NC}"
+echo -e "${RED}### RUN: Set up roles ###${NC}"
 ./vendor/bin/drush user:role:add 'nodehive_content_admin' admin
 echo -e "${CYAN}### Set up roles -> DONE ###${NC}"
 
-
 echo -e "\r\n"
-echo -e "${GREEN}### Installation complete,  you can now log-in using username: ${YELLOW}admin${NC} ${GREEN}and password:${NC} ${YELLOW}admin${NC} ${GREEN}###${NC}"
-
 echo -e "\r\n"
-echo -e "${CYAN}# Visit www.nodehive.com and docs.nodehive.com for further information. #${NC}"
-echo -e "${CYAN}## Happy Headless Coding and greets from the NETNODE team – www.netnode.ch. ##${NC}"
+cat << EOF
 
+#############################################################
+
+┏┓╻┏━┓╺┳┓┏━╸╻ ╻╻╻ ╻┏━╸   ╻ ╻┏━╸┏━┓╺┳┓╻  ┏━╸┏━┓┏━┓   ┏━╸┏┳┓┏━┓
+┃┗┫┃ ┃ ┃┃┣╸ ┣━┫┃┃┏┛┣╸    ┣━┫┣╸ ┣━┫ ┃┃┃  ┣╸ ┗━┓┗━┓   ┃  ┃┃┃┗━┓
+╹ ╹┗━┛╺┻┛┗━╸╹ ╹╹┗┛ ┗━╸   ╹ ╹┗━╸╹ ╹╺┻┛┗━╸┗━╸┗━┛┗━┛   ┗━╸╹ ╹┗━┛
+Scale your digital platform.
+
+#############################################################
+
+EOF
+
+echo -e "${RED}- www.nodehive.com ${NC}"
+echo -e "${RED}- www.nodehive.com/newsletter – Feature Updates${NC}"
+echo -e "${RED}- docs.nodehive.com – Developer documentation${NC}"
+echo -e "${RED}- www.netnode.ch – Makers of NodeHive Headless CMS ${NC}"
+echo -e "\r\n"
+
+echo -e "And now have fun and run 'ddev launch' to open the site in your browser."
+echo -e "${GREEN}Username: ${YELLOW}admin ${NC}"
+echo -e "${GREEN}Password:${NC} ${YELLOW}admin ${NC}"
+echo -e "\r\n"
+echo -e "\r\n"
